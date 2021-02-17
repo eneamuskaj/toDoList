68e001a16cf10a86fc9acca95db2abe79a9e8737
Enea Muskaj <eneamuskaj@gmail.com>
2021-02-17 13:16:13 -0600

    roll back

--- src/displayNotes.js
+++ src/displayNotes.js
@@ -1,25 +1,25 @@
-function displayNotes(numb) {
-  //delete previous content
-  document.getElementById("temporaryDiv").remove();
-  let redo = document.createElement("div");
-  redo.id = "temporaryDiv";
-  document.getElementById("displayContent").appendChild(redo);
+// function displayNotes(numb) {
+//   //delete previous content
+//   document.getElementById("temporaryDiv").remove();
+//   let redo = document.createElement("div");
+//   redo.id = "temporaryDiv";
+//   document.getElementById("displayContent").appendChild(redo);
 
-  const parent = document.getElementById("temporaryDiv");
+//   const parent = document.getElementById("temporaryDiv");
 
-  for (let i = 0; i < defaultList[numb].noteName.length; i++) {
-    let noteContainer = document.createElement("div");
-    let displayHeader = document.createElement("h1");
-    let displayDescription = document.createElement("p");
-    let doneButton = document.createElement("button");
-    displayHeader.innerHTML = defaultList[numb].noteName[i];
-    displayDescription.innerHTML = defaultList[numb].noteDescription[i];
+//   for (let i = 0; i < defaultList[numb].noteName.length; i++) {
+//     let noteContainer = document.createElement("div");
+//     let displayHeader = document.createElement("h1");
+//     let displayDescription = document.createElement("p");
+//     let doneButton = document.createElement("button");
+//     displayHeader.innerHTML = defaultList[numb].noteName[i];
+//     displayDescription.innerHTML = defaultList[numb].noteDescription[i];
 
-    parent.appendChild(noteContainer);
-    noteContainer.appendChild(displayHeader);
-    noteContainer.appendChild(displayDescription);
-    noteContainer.appendChild(doneButton);
-  }
-}
+//     parent.appendChild(noteContainer);
+//     noteContainer.appendChild(displayHeader);
+//     noteContainer.appendChild(displayDescription);
+//     noteContainer.appendChild(doneButton);
+//   }
+// }
 
-export { displayNotes };
+// export { displayNotes };
--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-var defaultList = [
-  {
-    name: "default",
-    noteName: ["Do the white clothes", "Do the green", "Dont Forget the black"],
-    noteDescription: [
-      "this is a brief description",
-      "this is a brief description",
-      "this is a brief description",
-    ],
-  },
-  {
-    name: "school",
-    noteName: ["school supplies", "notes", "calcumator"],
-    noteDescription: [
-      "this is a brief description",
-      "this is a brief description",
-      "this is a brief description",
-    ],
-  },
-];
+function initialize() {
+  var defaultList = [
+    {
+      name: "default",
+      noteName: [
+        "Do the white clothes",
+        "Do the green",
+        "Dont Forget the black",
+      ],
+      noteDescription: [
+        "this is a brief description",
+        "this is a brief description",
+        "this is a brief description",
+      ],
+    },
+    {
+      name: "school",
+      noteName: ["school supplies", "notes", "calcumator"],
+      noteDescription: [
+        "this is a brief description",
+        "this is a brief description",
+        "this is a brief description",
+      ],
+    },
+  ];
 
-function Item(name, noteName, noteDescription) {
-  this.name = name;
-  this.noteName = noteName;
-  this.noteDescription = noteDescription;
+  function Item(name, noteName, noteDescription) {
+    this.name = name;
+    this.noteName = noteName;
+    this.noteDescription = noteDescription;
+  }
+
+  var selectedProject = 0;
 }
-
-var selectedProject = 0;
 
 function displayProjects() {
   // delete previous content
@@ -103,6 +109,6 @@ function addNote() {
     displayNotes(selectedProject);
   });
 }
-
+initialize();
 displayProjects();
 addNote();
