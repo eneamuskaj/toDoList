dfd4e185fe309117f855e3428948f734cf9dd0b4
Enea Muskaj <eneamuskaj@gmail.com>
2021-02-17 13:17:27 -0600

    roll back2

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -1,24 +1,24 @@
-function displayProjects() {
-  // delete previous content
+// function displayProjects() {
+//   // delete previous content
 
-  var remover = document.getElementById("projectButtons");
-  remover.remove();
+//   var remover = document.getElementById("projectButtons");
+//   remover.remove();
 
-  const grabber = document.getElementById("content");
-  var projectButtons = document.createElement("div");
-  projectButtons.id = "projectButtons";
-  grabber.appendChild(projectButtons);
+//   const grabber = document.getElementById("content");
+//   var projectButtons = document.createElement("div");
+//   projectButtons.id = "projectButtons";
+//   grabber.appendChild(projectButtons);
 
-  for (let i = 0; i < defaultList.length; i++) {
-    let projectDisplay = document.createElement("button");
-    projectDisplay.innerHTML = defaultList[i].name;
-    projectButtons.appendChild(projectDisplay);
-    projectDisplay.addEventListener("click", function () {
-      displayNotes(i);
-      selectedProject = i;
-      console.log(selectedProject);
-    });
-  }
-}
+//   for (let i = 0; i < defaultList.length; i++) {
+//     let projectDisplay = document.createElement("button");
+//     projectDisplay.innerHTML = defaultList[i].name;
+//     projectButtons.appendChild(projectDisplay);
+//     projectDisplay.addEventListener("click", function () {
+//       displayNotes(i);
+//       selectedProject = i;
+//       console.log(selectedProject);
+//     });
+//   }
+// }
 
-export { displayProjects };
+// export { displayProjects };
