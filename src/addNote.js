f589280ec9becbab05233c79033eee0aab2bbdc5
Enea Muskaj <eneamuskaj@gmail.com>
2021-02-20 07:04:50 -0600

    added functionality

--- src/addNote.js
+++ src/addNote.js
@@ -1,31 +1,33 @@
-import { Item, defaultList, selectedProject } from "./index";
-import { displayNotes } from "./displayNotes";
+import { Item, newList, defaultList, selectedProject } from './index'
+import { displayNotes } from './displayNotes'
+import { displayProjects } from './displayProjects'
 export function addNote() {
-  const buttonClick = document.getElementById("firstButton");
+  const buttonClick = document.getElementById('firstButton')
 
-  buttonClick.addEventListener("click", function () {
-    const nameOfNote = document.getElementById("noteName").value;
-    const descriptionOfNote = document.getElementById("noteDescription").value;
+  buttonClick.addEventListener('click', function () {
+    const nameOfNote = document.getElementById('noteName').value
+    const descriptionOfNote = document.getElementById('noteDescription').value
+    const nameOfProject = document.getElementById('projectName').value
 
-    const newNote = new Item(
-      defaultList[selectedProject].name,
-      nameOfNote,
-      descriptionOfNote
-    );
+    let newNote = new Item(nameOfProject, nameOfNote, descriptionOfNote)
 
     for (let i = 0; i < defaultList.length; i++) {
       if (newNote.name === defaultList[i].name) {
-        defaultList[i].noteName.push(newNote.noteName);
-        defaultList[i].noteDescription.push(newNote.noteDescription);
+        defaultList[i].noteName.push(newNote.noteName)
+        defaultList[i].noteDescription.push(newNote.noteDescription)
+      } else {
+        newNote = new newList(nameOfProject, nameOfNote, descriptionOfNote)
+        defaultList.push(newNote)
+        console.log(defaultList)
       }
     }
 
     // delete previous content
-    document.getElementById("temporaryDiv").remove();
-    let redo = document.createElement("div");
-    redo.id = "temporaryDiv";
-    document.getElementById("displayContent").appendChild(redo);
-
-    displayNotes(selectedProject);
-  });
+    document.getElementById('temporaryDiv').remove()
+    let redo = document.createElement('div')
+    redo.id = 'temporaryDiv'
+    document.getElementById('displayContent').appendChild(redo)
+    displayProjects()
+    displayNotes(selectedProject)
+  })
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import { displayProjects } from "./displayProjects";
-import { addNote } from "./addNote";
+import { displayProjects } from './displayProjects'
+import { addNote } from './addNote'
 
-export var defaultList = [
+export let defaultList = [
   {
-    name: "default",
-    noteName: ["Do the white clothes", "Do the green", "Dont Forget the black"],
+    name: 'default',
+    noteName: ['Do the white clothes', 'Do the green', 'Dont Forget the black'],
     noteDescription: [
-      "this is a brief description",
-      "this is a brief description",
-      "this is a brief description",
+      'this is a brief description',
+      'this is a brief description',
+      'this is a brief description',
     ],
   },
   {
-    name: "school",
-    noteName: ["school supplies", "notes", "calcumator"],
+    name: 'school',
+    noteName: ['school supplies', 'notes', 'calcumator'],
     noteDescription: [
-      "this is a brief description",
-      "this is a brief description",
-      "this is a brief description",
+      'this is a brief description',
+      'this is a brief description',
+      'this is a brief description',
     ],
   },
-];
+]
 
 export function Item(name, noteName, noteDescription) {
-  this.name = name;
-  this.noteName = noteName;
-  this.noteDescription = noteDescription;
+  this.name = name
+  this.noteName = noteName
+  this.noteDescription = noteDescription
 }
 
-export var selectedProject = 0;
+export function newList(newProjname, newNoteName, newNoteDescription) {
+  this.name = newProjname
+  this.noteName = []
+  this.noteDescription = []
+}
 
-displayProjects();
-addNote();
+export var selectedProject = 0
+
+displayProjects()
+addNote()
